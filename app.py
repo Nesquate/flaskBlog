7f625459a515835f483e8134ef2e3d6d84316ea9
Nesquate <nesquate.100001@gmail.com>
2021-03-03 14:47:46 +0800

    Add nav and adjust test datas and templates

--- app.py
+++ app.py
@@ -1,30 +1,74 @@
-from flask import Flask, render_template, url_for, escape
+from flask import Flask, render_template, url_for, escape, redirect, abort
 core = Flask(__name__)
 
-from testdata import postData, title
+postData = [{
+    'id':'1',
+    'name':'test',
+    'content':'<p>this is a test post.</p>',
+    'tag':'test, post',
+    'category':'admin'
+},
+{
+    'id':'2',
+    'name':'test2',
+    'content':'<p>this is a test post2.</p>',
+    'tag':'test',
+    'category':'admin'
+}]
+
+def setSite():
+    homeUrl = url_for('home')
+    searchUrl = url_for('search')
+    siteData = [
+        {
+            'title':'Test Title',
+            'description':'A test site by Flask',
+            'url':homeUrl
+        },
+        [
+            {'name':'Home', 'url':homeUrl},
+            {'name':'Search', 'url':searchUrl}
+        ]
+    ]
+    return siteData
+
+    
+
+def genPostLink(postData):
+    for i in postData:
+        i['url'] = url_for('postPage', postid = i['id'])
 
 @core.route('/')
 def home():
-    for i in postData:
-        i['url'] = url_for('postPage', postid = i['id'])
-    
-    return render_template('post.html', postData=postData, viewPost=False, title=title, homelink=url_for('home'))
+    genPostLink(postData)
+    siteData = setSite()
+    return render_template('post.html', 
+        postData=postData,
+        siteData=siteData
+    )
 
-@core.route('/post/<postid>')
+@core.route('/post/<int:postid>')
 def postPage(postid):
-    return render_template("post.html", postData=postData[int(postid)-1], viewPost=True, title=title, homelink=url_for('home'))
+    siteData = setSite()
+    postSize = len(postData)
+    if postid <= postSize  and postid > 0:
+        return render_template('post.html', 
+            postData=postData,
+            postid=postid,
+            siteData=siteData
+        )
+    else:
+        return abort(404)
 
-@core.route('/tags')
-def tags():
-    return render_template('tags.html')
+@core.route('/post')
+@core.route('/categorie')
+@core.route('/tag')
+def returnToHome():
+    return redirect(url_for('home'))
 
 @core.route('/tag/<tag>')
 def tagPage(tag):
     return 'Now tag is :' + escape(tag)
-
-@core.route('/categories')
-def categories():
-    return render_template("categories.html")
 
 @core.route('/category/<category>')
 def categoryPage(category):
@@ -32,7 +76,11 @@ def categoryPage(category):
 
 @core.route('/search')
 def search():
-    return render_template('search.html')
+    siteData = setSite()
+    return render_template(
+        'search.html',
+        siteData=siteData
+    )
 
 @core.route('/search/<keyword>')
 def search_result(keyword):
