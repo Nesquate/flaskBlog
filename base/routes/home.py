6fc88548e439a96cdf0ad17331a37fbd37ceb1ec
Nesquate <nesquate.100001@gmail.com>
2021-03-05 17:25:38 +0800

    Try to create page gadget But it seems avaliable only for home

--- base/routes/home.py
+++ base/routes/home.py
@@ -6,7 +6,9 @@ from database import db
 def home():
     siteInfo = db.Site.query.first()
     posts = db.Posts.query.all()
+    tags = db.Tags.query.all()
+    categories = db.Categories.query.all()
     if posts:
-        return render_template('post.html', siteData=siteInfo, postData=posts)
+        return render_template('post.html', siteData=siteInfo, postData=posts, tags=tags, categories=categories)
     else:
-        return render_template('post.html', siteData=siteInfo)
+        return render_template('post.html', siteData=siteInfo, tags=tags, categories=categories)
